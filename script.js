75f0e9815933a8554db38ffb15cfb42c1cb22390
KarlMcSherry <karldylan@hotmail.com>
2016-12-11 15:11:47 +0000

    commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function fareEstimator () {
+    var dm_distance = document.getElementById ("distance").value,
+        dm_rate = document.getElementById ("rate").checked, 
+        dm_holiday = document.getElementById ("holiday").checked, 
+        estimatedCost;
+    
+    if (dm_distance <= 15) {
+        if (dm_rate === true || dm_holiday === true)
+            {
+                estimatedCost = ((dm_distance - 0.5) * 1.40);
+    } 
+        else {
+        estimatedCost = ((dm_distance - 0.5) * 1.10);    
+    }
+    }               
+    
+    else {
+        if (dm_rate === true || dm_holiday == true)
+            {
+            estimatedCost = (((15 - .5) * 1.40) +
+            ((dm_distance - 15) * 1.75))
+    }
+        else {
+        estimatedCost = (((15 - .5) * 1.10) + 
+            ((dm_distance - 15) * 1.45));    
+    }
+    }
+    var output = estimatedCost.toPrecision(4);
+    document.getElementById("estimatedFare").value = output;
+}
+
+function ageCheck (){
+    var age = prompt("How old are you?");
+    
+        if (age > 17){
+            alert ("Welcome!");
+        }
+        else {
+            alert ("Sorry, you are too young for this page.");
+            document.location.href = "index.html";
+        }
+}
+
+function changeStyle (style) {
+    document.getElementById("theme").setAttribute("href", style ) ;
+}
+
+
